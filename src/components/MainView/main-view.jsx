64447b76ad1f13fccc66312dd82d0636b5630bf1
Anthony <amach510@gmail.com>
2024-04-22 17:16:37 -0700

    import useEffect from react

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 
