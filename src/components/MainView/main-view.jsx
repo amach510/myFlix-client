43e82311eb21c89db7d1b695e23e130def646b83
Anthony <amach510@gmail.com>
2024-04-24 15:40:24 -0700

    created SignupView, integrated login and signup view with MainView

--- src/components/MainView/main-view.jsx
+++ src/components/MainView/main-view.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { MovieCard } from "../MovieCard/movie-card";
 import { MovieView } from "../MovieView/movie-view";
 import { LoginView } from "../LoginView/login-view";
+import { SignupView } from "../SignupView/signup-view";
 
 export const MainView = () => {
     const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -44,15 +45,17 @@ export const MainView = () => {
     //Token state variable
 
     if (!user) {
-    return (
-        <LoginView
-        onLoggedIn={(user, token) => {
-            setUser(user);
-            setToken(token);
-        }}
-        />
-    );
-    }
+        return (
+          <>
+            <LoginView onLoggedIn={(user, token) => {
+              setUser(user);
+              setToken(token);
+            }} />
+            or
+            <SignupView />
+          </>
+        );
+      }
 
     //onBackClick
     if (selectedMovie) {
