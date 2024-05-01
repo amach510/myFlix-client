14b3c2ce50be505cae6ec0d8015899a0a5f1436d
Anthony <amach510@gmail.com>
2024-04-30 17:39:32 -0700

    implemented card form bootstrap to movie-card

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,17 +1,21 @@
 import PropTypes from "prop-types";
+import { Button, Card } from "react-bootstrap";
 
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
-        <div
-            onClick={() => {
-                onMovieClick(movie);
-            }}
-        >
-            {movie.title}
-        </div>
-
-    );
-};
+        <Card className="h-100">
+          <Card.Img variant="top" src={movie.image} />
+          <Card.Body>
+            <Card.Title>{movie.Title}</Card.Title>
+            <Card.Text>{movie.Director.Name}</Card.Text>
+            <Button onClick={() => onBookClick(movie)} variant="link">
+              Open
+            </Button>
+          </Card.Body>
+        </Card>
+      );
+    };
+    
 
 // define all the props constraints for the MovieCard
 MovieCard.propTypes = {
