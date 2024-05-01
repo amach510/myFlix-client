9fedac95907470323afe0d0893efa8efd934db29
Anthony <amach510@gmail.com>
2024-04-30 17:59:11 -0700

    corrected syntax on onClick button under movie-card

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -8,7 +8,7 @@ export const MovieCard = ({ movie, onMovieClick }) => {
           <Card.Body>
             <Card.Title>{movie.Title}</Card.Title>
             <Card.Text>{movie.Director.Name}</Card.Text>
-            <Button onClick={() => onBookClick(movie)} variant="link">
+            <Button onClick={() => onMovieClick(movie)} variant="link">
               Open
             </Button>
           </Card.Body>
