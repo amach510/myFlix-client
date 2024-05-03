5625d5c5226b126fd8adde6584e5840645e87af1
Anthony <amach510@gmail.com>
2024-05-03 16:32:50 -0700

    replaced onMovieClick with Reacto Router in movie-card.jsx

--- src/components/MovieCard/movie-card.jsx
+++ src/components/MovieCard/movie-card.jsx
@@ -1,17 +1,20 @@
 import PropTypes from "prop-types";
 import { Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "./movie-card.scss";
 
-export const MovieCard = ({ movie, onMovieClick }) => {
+export const MovieCard = ({ movie }) => {
     return (
       <Card className="h-100 mt-5 card-shadow">
         <Card.Img variant="top card-img" src={movie.ImagePath} />
           <Card.Body>
             <Card.Title>{movie.Title}</Card.Title>
             <Card.Text>{movie.Director.Name}</Card.Text>
-            <Button onClick={() => onMovieClick(movie)} variant="link">
-              Open
-            </Button>
+            <Link to={`/movies/${encodeURIComponent(movie._id)}`}>
+                <Button variant="link">
+                    Open
+                </Button>
+            </Link>
           </Card.Body>
         </Card>
       );
@@ -23,5 +26,4 @@ MovieCard.propTypes = {
     movie: PropTypes.shape({
         Title: PropTypes.string,        
     }).isRequired,
-    onMovieClick: PropTypes.func.isRequired
 };
